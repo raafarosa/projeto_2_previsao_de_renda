87387c4e19d2ed8076ba70bd11a82c72fc2728b1
raafarosa <141688193+raafarosa@users.noreply.github.com>
2024-03-04 13:00:49 -0300

    update 2.2

--- projeto_2.py
+++ projeto_2.py
@@ -4,7 +4,7 @@ import io
 import numpy as np
 import pandas as pd
 
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 import matplotlib.pyplot as plt
